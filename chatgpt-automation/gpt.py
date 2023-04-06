25cc11f387207752a72210a8b032914d2f015176
dev-carvalho <marcos.antonio.carvalho@gmail.com>
2023-04-06 10:57:56 -0300

    Update by i3 in Thursday, 06 de April de 2023 at 10:57

--- chatgpt-automation/gpt.py
+++ chatgpt-automation/gpt.py
@@ -15,38 +15,41 @@ import argparse
 # Criar uma chave de acesso para api da OpenAI 
 # em https://platform.openai.com/account/api-keys 
 # e configurar o arquivo key.py
-from key import key_api # carregar a key_api do arquivo key.py
+from key import api_key # carregar a api_key do arquivo key.py
 
 parser = argparse.ArgumentParser()
 parser.add_argument("prompt", help="the prompt to send to the OpenAI API")
 parser.add_argument("file_name", help="Name of the file to save Python script")
 
 api_endpoint = "https://api.openai.com/v1/completions"
+#api_key = os.getenv("OPENAI_API_KEY")
 
-print(f'API Endpoint: {api_endpoint}')
-print(f'Key API: {key_api}')
-
+print("Python Automation with ChatGPT - Version 0.0.1")
+print("Designed by Marcos Antonio de Carvalho\n")
 
 args = parser.parse_args()
 
 request_headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer {api_key}'
+    'Authorization': 'Bearer ' + api_key
 }
 
 request_data = {
     "model": "text-davinci-003",
     "prompt": f"Write python script to {args.prompt}. Provide only code, no text",
     "max_tokens": 1000,
-    "temperature": 0.5 # define o nível ousadia da resposta (mais alto mais criativo)
+    "temperature": 0.8 # 0.5 0.2 # define o nível ousadia da resposta (mais alto mais criativo)
 }
 
 response = requests.post(api_endpoint, headers=request_headers, json=request_data)
 
 if response.status_code == 200:
+    print(f'API Endpoint..: {api_endpoint}')
+    print(f'API Access Key: {api_key}')
     #print(response.json()["choices"][0]["text"])
     response_text = response.json()["choices"][0]["text"]
     with open(args.file_name, "w") as file:
         file.write(response_text)
+    print(f'Generated code: {args.file_name}')
 else:
     print(f"Request failed with status code: {str(response.status_code)}")
