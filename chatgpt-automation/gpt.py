0ef7cb592a139f3e30c58ad4a0ac6ac0485e6dcf
dev-carvalho <marcos.antonio.carvalho@gmail.com>
2023-04-06 12:07:26 -0300

    Update by i3 in Thursday, 06 de April de 2023 at 12:07

--- chatgpt-automation/gpt.py
+++ chatgpt-automation/gpt.py
@@ -29,6 +29,9 @@ print("Designed by Marcos Antonio de Carvalho\n")
 
 args = parser.parse_args()
 
+print(f'API Endpoint..: {api_endpoint}')
+print(f'API Access Key: {api_key}')
+
 request_headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer ' + api_key
@@ -36,16 +39,16 @@ request_headers = {
 
 request_data = {
     "model": "text-davinci-003",
+    # 1 Token por minuto 
+    #
     "prompt": f"Write python script to {args.prompt}. Provide only code, no text",
-    "max_tokens": 1000,
+    "max_tokens": 4097, # https://platform.openai.com/docs/models/gpt-3-5
     "temperature": 0.8 # 0.5 0.2 # define o nível ousadia da resposta (mais alto mais criativo)
 }
 
 response = requests.post(api_endpoint, headers=request_headers, json=request_data)
 
 if response.status_code == 200:
-    print(f'API Endpoint..: {api_endpoint}')
-    print(f'API Access Key: {api_key}')
     #print(response.json()["choices"][0]["text"])
     response_text = response.json()["choices"][0]["text"]
     with open(args.file_name, "w") as file:
